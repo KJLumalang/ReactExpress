87958fdc524718c71b7c8aab846c26faa9ec82b2
Kim Jeiro Lumalang <102845013+KJLumalang@users.noreply.github.com>
2024-09-13 16:25:56 +0800

    Added sticky facebook icon

--- kawaii-frontend/src/pages/Home.js
+++ kawaii-frontend/src/pages/Home.js
@@ -28,6 +28,16 @@ const HomePage = () => {
 
 	return (
 		<div className="overflow-hidden">
+		{/* Socials Sticky */}
+		<div className="fixed flex top-4 right-4 z-50">
+			<a href="javascript:;" class="p-2 rounded-lg flex items-center justify-center transition-all duration-500 hover:border-gray-100 hover:bg-gray-100 bg-blue-600 text-white hover:bg-blue-700">
+				<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 71 72" fill="none">
+					<path
+						d="M46.4233 38.6403L47.7279 30.3588H39.6917V24.9759C39.6917 22.7114 40.8137 20.4987 44.4013 20.4987H48.1063V13.4465C45.9486 13.1028 43.7685 12.9168 41.5834 12.8901C34.9692 12.8901 30.651 16.8626 30.651 24.0442V30.3588H23.3193V38.6403H30.651V58.671H39.6917V38.6403H46.4233Z"
+						fill="#ffffff" />
+				</svg>
+			</a>
+		</div>
 		{/* Introduction Section */}
 		<motion.section
 			className="h-screen flex flex-col justify-center items-center bg-cover bg-center px-4 md:px-8"
