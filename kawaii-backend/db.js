cf1e8dc817762bc36deb3f5d70525c772603cb4b
Kim Jeiro Lumalang <102845013+KJLumalang@users.noreply.github.com>
2024-09-23 09:21:18 +0800

    Add node_modules to .gitignore and remove from tracking

--- kawaii-backend/db.js
+++ kawaii-backend/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
-  port: '3307',
+  port: '3306',
   user: 'root',
-  password: 'password',
+  password: '',
   database: 'kawaii'
 });
 
